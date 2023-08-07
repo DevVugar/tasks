4afe99b1d548db40641bfc8e54f9d26ac11a95bb
DevVugar <rehimovvuqarrrr@gmail.com>
2023-08-07 15:45:22 +0400

    was created word1

--- demo2/src/main/java/com/mycompany/demo2/Demo2.java
+++ demo2/src/main/java/com/mycompany/demo2/Demo2.java
@@ -12,6 +12,6 @@ package com.mycompany.demo2;
 public class Demo2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("demo 2");
     }
 }
